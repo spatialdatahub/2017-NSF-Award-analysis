9ce5b8a694e2dcb47c1260348e12ded4d612b128
patcurry <currypat1985@gmail.com>
2018-07-30 10:28:20 +0200

    changed the y axis ticks, y axis disclaimer and removed x axis ticks

--- analyses/plot_awards.py
+++ analyses/plot_awards.py
@@ -45,9 +45,9 @@ one_fifth_index = index[fourfifths:]
 # gather subplots for axis labels
 fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True, sharey=True)
 
-fig.text(0.5, 0.02,
+fig.text(0.5, 0.04,
          'Grant index sorted from largest to smallest', ha='center')
-fig.text(0.02, 0.5, 'Grant size ($)',
+fig.text(0.06, 0.5, 'Grant size in millions of dollars',
          va='center', rotation='vertical')
 
 # top subplot
@@ -63,14 +63,22 @@ plt.plot(second_half_index, second_half_awards, 'k-',
 plt.fill_between(second_half_index, second_half_awards,
                  0, facecolor='red', alpha=0.5)
 
+# turn off x tick marks
+plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+
 # set axis limits
 axes = plt.gca()
 axes.set_xlim([0, 12000])
 axes.set_ylim([0, 5000000])
 
+# set y axis ticks
+y_tick_locs = [0, 1000000, 2000000, 3000000, 4000000, 5000000]
+y_tick_labels = [0, 1, 2, 3, 4, 5]
+plt.yticks(y_tick_locs, y_tick_labels)
+
 # set x-axes tick marks
-x = [0, 3000, 6000, 9000, 12000]
-plt.xticks(range(min(x), max(x)+1, 3000))
+#x = [0, 3000, 6000, 9000, 12000]
+#plt.xticks(range(min(x), max(x)+1, 3000))
 
 # put legend into upper subplot
 plt.legend(loc='upper right', edgecolor='w')
@@ -83,11 +91,11 @@ plt.axvline(x=half, color='k', linestyle='--', lw=1)
 plt.title('NSF research grants awarded in 2017')
 
 # make text describing y axis
-grant_size_string="""While the y axis stops at
-$5 million the largest grant
-was over $209 million"""
+grant_size_string="""To see smaller sized grant more clearly
+the y axis stops at $5 million, but the
+largest grant was over $ 209 million"""
 
-plt.text(1000,4500000, grant_size_string, verticalalignment='top')
+plt.text(500,4500000, grant_size_string, verticalalignment='top')
 
 # bottom subplot
 plt.subplot(2, 1, 2)
@@ -106,9 +114,19 @@ axes = plt.gca()
 axes.set_xlim([0, 12000])
 axes.set_ylim([0, 5000000])
 
+# set y axis ticks
+y_tick_locs = [0, 1000000, 2000000, 3000000, 4000000, 5000000]
+y_tick_labels = [0, 1, 2, 3, 4, 5]
+plt.yticks(y_tick_locs, y_tick_labels)
+
+#plt.xticks(range(min(x), max(x)+1, 3000))
+
 # set x-axes tick marks
-x = [0, 3000, 6000, 9000, 12000]
-plt.xticks(range(min(x), max(x)+1, 3000))
+#x = [0, 3000, 6000, 9000, 12000]
+#plt.xticks(range(min(x), max(x)+1, 3000))
+
+# turn off x tick marks
+plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
 
 plt.axvline(x=fourfifths, color='k', linestyle='--', lw=1)
 
